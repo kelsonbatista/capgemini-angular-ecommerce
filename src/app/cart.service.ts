08a8ac025618c04cc66c7e7574d682aea4f7eb21
kelsonbatista <kelsonbatista@gmail.com>
2022-11-05 15:43:35 -0300

    feat: added cart component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,12 +10,19 @@ export class CartService {
   constructor() { }
 
   getCart() {
-    return JSON.parse(localStorage.getItem("cart") || "");
+    return JSON.parse(localStorage.getItem("cart") || "[]");
   }
 
   addToCart(produto: IProdutoCarrinho) {
     this.items.push(produto);
-    localStorage.setItem("cart", JSON.stringify(this.items))
+    localStorage.setItem("cart", JSON.stringify(this.items));
+  }
+
+  deleteCartItem(produtoId: number) {
+    console.log(`produto id cart service: ${produtoId}`);
+    console.log(this.items);
+    this.items = this.items.filter(item => item.id != produtoId);
+    localStorage.setItem("cart", JSON.stringify(this.items));
   }
 
   deleteCart() {
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
+import { IProdutoCarrinho } from '../produtos';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartItems: IProdutoCarrinho[] = [];
+  total: number = 0;
 
-  constructor() { }
+  constructor(
+    public cartService: CartService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.cartItems = this.cartService.getCart();
+    this.getTotal();
+  }
+
+  deleteCartItem(produtoId: number) {
+    this.cartItems = this.cartItems.filter(item => item.id != produtoId);
+    this.cartService.deleteCartItem(produtoId);
+    this.getTotal();
+  }
+
+  getTotal() {
+    this.total = this.cartItems.reduce((prev, curr) => prev + (curr.preco * curr.quantidade), 0)
+  }
+
+  buyItNow() {
+    alert("Parabéns! Compra realizada com sucesso!");
+    this.cartService.deleteCart();
+    this.router.navigate(["produtos"]);
   }
 
 }
