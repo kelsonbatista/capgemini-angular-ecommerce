aa11cc44dca21c71b7cc5e07a5ba9a28901b4914
kelsonbatista <kelsonbatista@gmail.com>
2022-11-05 11:25:13 -0300

    feat: added products service

--- src/app/produtos/produto/produto.component.ts
+++ src/app/produtos/produto/produto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-produto',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto.component.css']
 })
 export class ProdutoComponent implements OnInit {
+  produto: IProduto | undefined;
+  quantidade: Number = 1;
 
-  constructor() { }
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
   }
 
 }
